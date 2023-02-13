0cb003c10a23a91b2bf5cb7d28c068c33c15d007
Montu-Gohain <leomontugohain@gmail.com>
2023-02-13 22:15:29 +0530

    Initial upload to github

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./App.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>Created with 💗</span>
+      <a href=''>
+        <span>Source Code</span>
+      </a>
+    </footer>
+  );
+};
+
+function App() {
+  const [link, setLink] = useState("");
+  const [got, setGot] = useState(null);
+  const [copied, setCopied] = useState(false);
+  const generateLink = () => {
+    let downloadLink;
+    if (link.length < 1) return alert("Please enter the link.");
+    let newLink = link.substring(32).split("/")[0];
+    downloadLink = `https://drive.google.com/uc?export=download&id=${newLink}`;
+    setGot(downloadLink);
+  };
+
+  return (
+    <div className='main-container'>
+      <h1>Paste your google Drive link</h1>
+      <input
+        type='text'
+        className='input-box'
+        onChange={(e) => setLink(e.target.value)}
+      />
+      <button className='generate-btn' onClick={generateLink}>
+        Generate Link
+      </button>
+      <section style={{ marginTop: "20px" }}>
+        {got ? (
+          <CopyToClipboard text={got} onCopy={() => setCopied(true)}>
+            <span className='copy-btn'>Copy download link</span>
+          </CopyToClipboard>
+        ) : null}
+      </section>
+      {copied && (
+        <h5 style={{ color: "red", fontSize: "large" }}>Link Copied 🖖</h5>
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
